cba42ca1154d2886c9def4c44e7f9597c55785c8
Shivam Maurya <mauryashivam2245@gmail.com>
2026-06-28 21:13:15 +0530

    link update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const request = document.querySelector('.request');
 const service = document.querySelector('.view_services');
 
 request.addEventListener('click', ()=> {
-    window.location.href = "../Contact/contact.html"
+    window.location.href = "Contact/contact.html"
 })
 
 service.addEventListener('click', ()=> {
-    window.location.href = "../servicess/service.html"
+    window.location.href = "servicess/service.html"
 })
