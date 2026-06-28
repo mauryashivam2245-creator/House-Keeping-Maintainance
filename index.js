dc3564e614ef2aad0fdef9904704478995c22c35
Shivam Maurya <mauryashivam2245@gmail.com>
2026-06-28 12:48:06 +0530

    complete first page

---
+++ index.js
@@ -0,0 +1,10 @@
+const request = document.querySelector('.request');
+const service = document.querySelector('.view_services');
+
+request.addEventListener('click', ()=> {
+    window.location.href = "../Contact/contact.html"
+})
+
+service.addEventListener('click', ()=> {
+    window.location.href = "../servicess/service.html"
+})
